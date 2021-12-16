bf7ddcad97aa621dea1d5d3204d0a5373ea1802e
Zachary Fong <mrfong88a@outlook.com>
2021-12-16 03:12:37 -0800

    completed

--- PhunProject/StarWarAPI/StarWarEndPoint.swift
+++ PhunProject/StarWarAPI/StarWarEndPoint.swift
@@ -16,7 +16,7 @@ extension StarWarEndpoint: RequestBuilder {
     var urlRequest: URLRequest {
         switch self {
         case .StarWarList:
-            guard let url = URL(string: "https://raw.githubusercontent.com/phunware-services/dev-interview-homework/master/feed.json")
+            guard let url = URL(string: "https://json")
             else {preconditionFailure("Invalid URL format")}
             let request = URLRequest(url: url)
             return request
